95c5b42b747988e478738731e9d29e582ccaf2aa
joseph.yoon <joseph.yoon@kakaocorp.com>
2018-02-13 17:35:53 +0900

    build faile 나는 거 수정

--- src/test/java/com/kakaocorp/commerce/khaleesi/TestBase.java
+++ src/test/java/com/kakaocorp/commerce/khaleesi/TestBase.java
@@ -7,9 +7,13 @@
 
 package com.kakaocorp.commerce.khaleesi;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 /**
  * 베이스 테스트
@@ -19,4 +23,8 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TestBase {
+    @Test
+    public void name() {
+        assertThat(true, is(true));
+    }
 }
