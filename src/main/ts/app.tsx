811c8e4fa89c60859be9d21b38e04e1ffc79f646
joseph.yoon <joseph.yoon@kakaocorp.com>
2018-03-20 19:09:34 +0900

    메뉴까지만... 스크립트 추가한 것은 나중에 빼보자.

--- src/main/ts/app.tsx
+++ src/main/ts/app.tsx
@@ -6,13 +6,29 @@
  */
 
 import * as React from 'react';
-import {Button} from 'reactstrap';
+import * as rs from 'reactstrap';
 
 export default class App extends React.Component<{}>{
   render(){
     return (
-      <div>
-        <Button color="danger">Click</Button>
+      <div className="container-fluid">
+        <header>
+          <rs.Navbar fixed={"top"} dark={true} expand={"lg"} toggleable={true} className="pink" >
+            <rs.NavbarBrand href="#"><strong>Khaleesi</strong></rs.NavbarBrand>
+            <rs.NavbarToggler data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </rs.NavbarToggler>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+              <rs.Nav className="mr-auto" navbar={true}>
+                <rs.NavItem active={true}><rs.NavLink href="#">Home</rs.NavLink></rs.NavItem>
+                <rs.NavItem><rs.NavLink href="#">Schedule</rs.NavLink></rs.NavItem>
+                <rs.NavItem><rs.NavLink href="#">Worker</rs.NavLink></rs.NavItem>
+                <rs.NavItem><rs.NavLink href="#">Member</rs.NavLink></rs.NavItem>
+                <rs.NavItem><rs.NavLink href="#">Log</rs.NavLink></rs.NavItem>
+              </rs.Nav>
+            </div>
+          </rs.Navbar>
+        </header>
       </div>
     );
   }
--- src/main/ts/index.tsx
+++ src/main/ts/index.tsx
@@ -8,7 +8,7 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'mdbootstrap/css/mdb.min.css'
+import 'mdbootstrap/css/mdb.min.css';
 import App from './app';
 
 const ROOT = document.querySelector('#main');
